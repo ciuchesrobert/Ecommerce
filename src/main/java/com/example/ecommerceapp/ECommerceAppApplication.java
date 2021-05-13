3722af71bb8db9f8c0fac2f9b75bfcb6c1abe70a
ciuches.robert <ciuches.robert@yahoo.com>
2021-05-13 15:53:16 +0300

    initial commit

--- src/main/java/com/example/ecommerceapp/ECommerceAppApplication.java
+++ src/main/java/com/example/ecommerceapp/ECommerceAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommerceappApplication {
+public class ECommerceAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcommerceappApplication.class, args);
+		SpringApplication.run(ECommerceAppApplication.class, args);
 	}
 
 }
